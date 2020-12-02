ca7d56337820ef7c068b7fed0793999fa61c33b6
Rob Wagner <13954303+robertwayne@users.noreply.github.com>
2020-12-02 15:10:06 -0500

    Updated timer to daily @ 7AM. Now deletes all files.
    @TODO: copy certain filetypes to respective folders

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::{
     time::Duration,
 };
 
+// use fs_extra;
+
 use clokwerk::{Scheduler, TimeUnits};
 
 fn get_files(dir: &Path) -> Result<Vec<PathBuf>, io::Error> {
@@ -29,29 +31,38 @@ fn parse_files(dir: &Path) {
             .unwrap();
 
         match ext {
-            "exe" => destroy(file),
-            "msi" => destroy(file),
-            "zip" => destroy(file),
-            _ => {}
+            // "exe" | "msi" | "zip" => destroy_files(file),
+            // "png" | "jpg" | "jpeg" | "bmp" | "gif" => copy_files(file),
+            _ => destroy_files(file),
         }
     }
 }
 
-fn destroy(file: PathBuf) {
+fn destroy_files(file: PathBuf) {
     fs::remove_file(file).unwrap()
 }
+
+// fn copy_files(file: PathBuf, dir: &Path) {
+//     let to_docs = Path::new(&fs::read_dir("\\Documents").unwrap());
+// }
 
 fn main() {
     let dir =
         env::var("USERPROFILE").expect("Could not find USERPROFILE in environment variables.");
+    let cwd = fs::read_dir(dir.clone() + "\\Documents");
+    println!("{:?}", (dir.clone(), cwd));
+
+    // run once initially before setting up the scheduler, so we can give ownership of `dir` away
+    parse_files(format!("{}/{}", dir, "Downloads").as_ref());
 
     let mut scheduler = Scheduler::new();
     scheduler
-        .every(7.days())
+        .every(1.day())
+        .at("7:00 AM")
         .run(move || parse_files(format!("{}/{}", dir, "Downloads").as_ref()));
 
     loop {
         scheduler.run_pending();
-        thread::sleep(Duration::from_secs(3600));
+        thread::sleep(Duration::from_millis(1000))
     }
 }
